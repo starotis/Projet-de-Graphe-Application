f23da82b8ded5d142d3c6d2a56e6dd5e59d92106
Melshea01 <98013688+Melshea01@users.noreply.github.com>
2022-04-01 15:50:24 +0200

    Add files via upload

---
+++ tp1/tp1/src/graphe/Algorithme.java
@@ -0,0 +1,89 @@
+package graphe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class Algorithme {
+	
+	public void AlgoDjikstra (Graphe graphe,Vertex vertex) {
+		// Initialisation
+		
+	     ArrayList<Vertex> Z = (ArrayList<Vertex>) graphe.getListeSommets().clone();
+	     HashMap <Vertex, Double> Dist = new  HashMap<Vertex, Double> ();
+	     HashMap <Vertex, Double> Dist2 = new  HashMap<Vertex, Double> ();
+	     
+	     Dist2.put(vertex,0.00); 
+	     Z.remove(vertex); 
+
+	     for(Vertex v: Z) 
+	        { 
+	    	 Dist2.put(v,Double.POSITIVE_INFINITY);
+	        }
+	     
+	     
+	     Vertex v_successeur_depart;
+	     for(int i=0;i<graphe.listeAdjacence.get(vertex.getSommet()).size();i++)
+	     {
+	    	 v_successeur_depart = graphe.getListeSommets().get(graphe.listeAdjacence.get(vertex.getSommet()).get(i).getSommetTerminal() );	 
+	    	 Dist2.put(v_successeur_depart, graphe.listeAdjacence.get(vertex.getSommet()).get(i).getValeurs()[0]);
+	     	 //System.out.println("Test" + Dist2.get(v_successeur_depart));
+	     }
+	     
+	      
+	       while (Z.isEmpty()==false) {
+	    	   
+	    	   for ( Vertex v: Z)
+	    	   {
+	    		   Dist.put(v,Dist2.get(v)); 
+	    	   }
+	    	   
+    		   Double min = Collections.min(Dist.values());
+	    	   Vertex element = getSingleKeyFromValue(Dist,min);
+	    	   Z.remove(element); 
+	    	   Vertex v_successeur_element;
+	    	   
+	    	   for(int i=0;i<graphe.listeAdjacence.get(element.getSommet()).size();i++)
+	   	     		{
+			   	    	v_successeur_element = graphe.getListeSommets().get(graphe.listeAdjacence.get(element.getSommet()).get(i).getSommetTerminal());	 
+			        	double d1 = Dist2.get(element);
+			        	double d2 = graphe.listeAdjacence.get(element.getSommet()).get(i).getValeurs()[0]; 
+			        	if(d1+d2 < Dist2.get(v_successeur_element)) {
+			        		Dist.replace(v_successeur_element,d1+d2);
+		        			Dist2.replace(v_successeur_element,d1+d2);	
+			        	}
+	   	     		}
+	    	   
+		    	Dist.clear();
+
+	        }
+	       
+	    Set listKeys=Dist2.keySet();
+   		Iterator iterateur=listKeys.iterator();   
+	       while(iterateur.hasNext())
+   		{
+   			Object key= iterateur.next();
+   			System.out.println (key+"=>"+Dist2.get(key));
+   		}
+
+	}
+	
+	public void AlgoAetoile (Graphe graphe,Vertex vertex) {
+		
+	}
+	
+	public static <K, V> K getSingleKeyFromValue(Map<K, V> map, V value) {
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+	
+	
+}
--- tp1/tp1/src/graphe/Vertex.java
+++ tp1/tp1/src/graphe/Vertex.java
@@ -2,19 +2,45 @@ package graphe;
 
 public class Vertex {
 	String nom;
-	int sommet;
-	double dist;
-	
+	int num_sommet;
+	double longitude;
+	double latitude; 
+	 
 	public Vertex(String nom, int s)
 	{
 		this.nom = nom;
-		this.sommet = s;
+		this.num_sommet = s;
 	}
+	
 	public String getNom() {
 		return nom;
 	}
 	public int getSommet() {
-		return sommet;
+		return num_sommet;
+	}
+	
+	public Vertex(String nom, int s, double longitude, double latitude)
+	{
+		this.nom = nom;
+		this.num_sommet = s;
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
 	}
 	
 	
